4c417459b12d39ef67ee471ebac4338ae2e43207
Ivanna Kosiak <ikosjak@gmail.com>
2018-02-11 19:38:04 +0200

    Task about leap year with one operator if-else.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int year;
+        boolean isLeap;
+        System.out.println("Введіть рік");
+        Scanner sc = new Scanner(System.in);
+        year = sc.nextInt();
+
+        if (((year % 4) == 0) && ((year % 100) != 0) || ((year % 400) == 0))
+            isLeap = true;
+        else
+            isLeap = false;
+
+        System.out.println("Year is leap - " + isLeap);
     }
 }
