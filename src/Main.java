ca6079e82915347bead27dea96c6329cf113a073
Ivanna Kosiak <ikosjak@gmail.com>
2018-02-11 13:47:54 +0200

    First task about flower bed in circle shape.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Введіть радіус в сантиметрах");
+        Scanner sc = new Scanner(System.in);
+        double rad = sc.nextDouble();
+
+        double per = 2*Math.PI*rad;
+        double area = Math.PI*rad*rad;
+
+        System.out.println("Perimeter of flower bed in cm is: " + per + "\nArea of flower bed in cm is: " + area);
+
     }
 }
