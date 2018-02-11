fc7c1125f4bacc5038a25bb1a37b3d12f1e96ba6
Ivanna Kosiak <ikosjak@gmail.com>
2018-02-11 20:02:00 +0200

    Task about display the pattern like a pyramid using asterisk.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int year;
+        /*int year;
         boolean isLeap;
         System.out.println("Введіть рік");
         Scanner sc = new Scanner(System.in);
@@ -14,6 +14,19 @@ public class Main {
         else
             isLeap = false;
 
-        System.out.println("Year is leap - " + isLeap);
+        System.out.println("Year is leap - " + isLeap);*/
+
+        int number;
+        System.out.println("Введіть непарне число");
+        Scanner sc = new Scanner(System.in);
+        number = sc.nextInt();
+        int num = (int)(number/2);
+        for (int i = 0; i < number+1;){
+            for(int j = 0; j < num; j++) System.out.print(" ");
+            for (int k = 0; k < i+1; k++) System.out.print("*");
+            System.out.println("");
+            num--;
+            i+=2;
+        }
     }
 }
