2933ff7a76010c146a3d4d704f7a91669da7f97f
Ivanna Kosiak <ikosjak@gmail.com>
2018-02-11 22:56:57 +0200

    Enum

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String country;
+        System.out.println("Введіть країну");
+        Scanner sc = new Scanner(System.in);
+        country = sc.next();
+        Continents continent = Continents.NEVERLAND; //знаю що так не можна але по іншому не компілюється
+        switch (country){
+            case "Ukraine" : case "Poland" : case "Germany" : case "Great Britan" :
+                continent = Continents.EUROPE; break;
+            case "Peru" : case "Brasilia" : case "Argentina" :
+                continent = Continents.SOUTH_AMERICA; break;
+            case "USA" : case "Canada" : case "Mexico" :
+                continent = Continents.NORTH_AMERICA; break;
+            case "Kenya" : case "Libya" : case "Cameroon" :
+                continent = Continents.AFRICA; break;
+            case "Australia" : case "Fiji" : case "Samoa" : case "New Zeland" :
+                continent = Continents.AUSTRALIA_AND_OCEANIA; break;
+            case "China" : case "Korea" : case "Japan" : case "Thailand" :
+                continent = Continents.ASIA; break;
+            default:
+                System.out.println("Даної країни немає в базі даних або не існує");
+        }
+
+        System.out.println(country + " is in " + continent);
     }
 }
