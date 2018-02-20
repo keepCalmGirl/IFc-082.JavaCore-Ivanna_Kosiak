d7049a6a4530a6e85a9a4ac2250f14838cd0d0a0
Ivanna Kosiak <ikosjak@gmail.com>
2018-02-20 10:49:40 +0200

    Перевірка чи даний рядок містить вказаний підрядок. Переведення всіх перших букв слів заданого речення to upper case.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //practical tasks
+	String str1 = "Max";
+	String str2 = "Max Demian";
+        System.out.println(str2.contains(str1));
+
+        System.out.println(makeAllFirstLetterUpper("Come here and help yourself"));
+
+    }
+    public static String makeAllFirstLetterUpper(String str) {
+        String tempArray[] = str.split(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < tempArray.length; i++) {
+            char tempCh = Character.toUpperCase(tempArray[i].charAt(0));
+            tempArray[i] = tempArray[i].replace(tempArray[i].charAt(0), tempCh);
+            stringBuilder.append(tempArray[i]);
+            stringBuilder.append(" ");
+       }
+       return stringBuilder.toString();
     }
 }
